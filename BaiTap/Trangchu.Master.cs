8ade659415825d30a2b9cf4f8231cfdeaf328dc5
ACER <ACER@THANG>
2025-05-20 16:45:33 +0700

    đã thêm rất nhiều đừng quan tâm

--- BaiTap/Trangchu.Master.cs
+++ BaiTap/Trangchu.Master.cs
@@ -12,31 +12,47 @@ using System.Web.UI.WebControls.WebParts;
 
 namespace BaiTap
 {
-    
+
     public partial class Trangchu : System.Web.UI.MasterPage
     {
-        SqlConnection conn = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=thang;Integrated Security=True");
-        public DataTable quangcao1;
+       private SqlConnection conn = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=thang;Integrated Security=True");
+        
         public MenuItem menuitem;
+
+        public DataTable quangcao1
+        {
+            get { return ViewState["quangcao1"] as DataTable; }
+            set { ViewState["quangcao1"] = value; }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 dsqc();
+                LoadSanPham();
             }
-            else
-            {
-                dsqc();
-            }    
         }
+
         protected void dsqc()
         {
-            string masp = "select * from sanpham where quangcao = 'true'";
-            SqlDataAdapter da = new SqlDataAdapter(masp, conn);
-            quangcao1 = new DataTable();
-            da.Fill(quangcao1);
+            string query = "SELECT masanpham, hinh FROM sanpham WHERE quangcao = 'true'";
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            quangcao1 = dt;
         }
-            
+
+      
+        private void LoadSanPham()
+        {
+            string query = "SELECT masanpham, tensanpham, hinh, dongia, 34990000 AS giagoc, 13 AS giamgia, 305000 AS giamthem FROM SanPham";
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            rptSanPham.DataSource = dt;
+            rptSanPham.DataBind();
+        }
 
     }
 }
--- BaiTap/Trangchu.Master.designer.cs
+++ BaiTap/Trangchu.Master.designer.cs
@@ -33,6 +33,33 @@ namespace BaiTap
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+
+        /// <summary>
+        /// ContentPlaceHolder2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+
+        /// <summary>
+        /// rptSanPham control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptSanPham;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
